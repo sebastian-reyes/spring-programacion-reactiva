e0319176483e0f64d5841dcfeb9a6c6214bb6cd2
sebastian-reyes <sebastianreyes2105@gmail.com>
2024-07-28 12:44:01 -0500

    Iniciando repo proyecto padre

---
+++ src/test/java/com/sreyes/reactiva/CursoProgramacionReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreyes.reactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoProgramacionReactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
